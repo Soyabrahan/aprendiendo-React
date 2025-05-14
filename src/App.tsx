9bd95fd2c73a40913f32a493f9a658dd2585c226
Abrahan <abrahanramirez2410@gmail.com>
2025-05-13 20:40:00 -0400

    agregando funcion a list

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import List from "./components/List";
 
 function App() {
   const list = ["abrahan", "victor", "saul"];
-
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo", elemento);
+  };
   return (
     <Card>
       <CardBody title="TITULOOOO" text="texto de la  cartaa" />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
     </Card>
   );
 }
