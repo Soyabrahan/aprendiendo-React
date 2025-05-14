2f73b3aeb6409b738e6eafb06a3a4d908f24b7c0
Abrahan <abrahanramirez2410@gmail.com>
2025-05-13 20:58:46 -0400

    agregando validacion a lista

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
 function App() {
-  const list = ["abrahan", "victor", "saul"];
+  const list: string[] = ["samuel", "abrahan"];
+
   const handleSelect = (elemento: string) => {
     console.log("imprimiendo", elemento);
   };
+
+  const contenido = list.length ? (
+    <List data={list} onSelect={handleSelect} />
+  ) : (
+    "Sin elementos para mostrar"
+  );
+
   return (
     <Card>
       <CardBody title="TITULOOOO" text="texto de la  cartaa" />
-      <List data={list} onSelect={handleSelect} />
+      {contenido}
     </Card>
   );
 }
